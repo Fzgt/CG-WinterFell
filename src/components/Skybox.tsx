d0b232eb97d748db79bf417fe7ff407013c587ef
Fzgt <fzgt320@gmail.com>
2025-03-01 00:26:36 +1100

    feat: add stars

--- src/components/Skybox.tsx
+++ src/components/Skybox.tsx
@@ -1,20 +1,29 @@
 import * as THREE from 'three';
-import { useTexture } from '@react-three/drei'
+import { useTexture, Stars } from '@react-three/drei'
+import { useLayoutEffect } from 'react';
 
 
 const Skybox = () => {
     const galaxyTexture = useTexture('/textures/galaxy.jpg')
+    useLayoutEffect(() => {
+        galaxyTexture.wrapS = galaxyTexture.wrapT = THREE.RepeatWrapping;
+        galaxyTexture.repeat.set(2, 2);
+    })
 
     return (
-        <mesh>
-            <sphereGeometry args={[800, 32, 32]} />
-            <meshPhongMaterial
-                emissive={0xff2190}
-                side={THREE.BackSide}
-                emissiveIntensity={0.1}
-                map={galaxyTexture}
-            />
-        </mesh>
+        <>
+            <Stars factor={80} radius={280} saturation={0} count={10000} fade speed={3} />
+
+            <mesh>
+                <sphereGeometry args={[800, 32, 32]} />
+                <meshPhongMaterial
+                    emissive={0xff2190}
+                    side={THREE.BackSide}
+                    emissiveIntensity={0.1}
+                    map={galaxyTexture}
+                />
+            </mesh>
+        </>
     )
 }
 
