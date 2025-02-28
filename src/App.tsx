3788968ccc987b3a59672702f3f0847a1a3d3b96
Fzgt <fzgt320@gmail.com>
2025-02-28 22:41:29 +1100

    feat: add skybox & orbitControl

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei';
+import Skybox from './components/Skybox';
 
 const App = () => <Canvas>
     <ambientLight />
+    <OrbitControls />
+    <Skybox />
 </Canvas>
 
 export default App;
