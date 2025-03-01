20197411461e4ac40ebacf4a079cdabfc15ba694
Fzgt <62455458+Fzgt@users.noreply.github.com>
2025-03-01 21:45:34 +1100

    refactor: plane component & shipSpeed

--- src/components/Ship.tsx
+++ src/components/Ship.tsx
@@ -2,6 +2,7 @@ import { useAnimations, useGLTF, PerspectiveCamera, } from "@react-three/drei"
 import { Suspense, useRef, useEffect, useImperativeHandle, forwardRef } from "react"
 import * as THREE from 'three'
 import { useFrame } from "@react-three/fiber"
+import { shipSpeed } from "../constants"
 
 interface ShipModelRef {
     shipModel: React.RefObject<THREE.Group>
@@ -10,20 +11,24 @@ interface ShipModelRef {
 const ShipModel = forwardRef<ShipModelRef>(
 
     (_props, ref) => {
-        const group = useRef<THREE.Group>(null)
+        const groupRef = useRef<THREE.Group>(null)
 
         const { scene, animations } = useGLTF('/models/bull_dog/scene.gltf')
-        const { actions, names } = useAnimations(animations, group)
+        const { actions, names } = useAnimations(animations, groupRef)
         useEffect(() => {
             actions[names[0]]?.play();
         }, []);
 
         useImperativeHandle(ref, () => ({
-            shipModel: group
+            shipModel: groupRef
         }))
 
+        useFrame(() => {
+            groupRef.current?.position.set(0, 3, groupRef.current!.position.z - shipSpeed);
+        })
+
         return (
-            <group ref={group} scale={2} position={[0, 1, 0]}>
+            <group ref={groupRef} scale={2} position={[0, 1, -20]}>
                 <primitive
                     object={scene}
                     rotation={[0, Math.PI, 0]}
@@ -32,7 +37,6 @@ const ShipModel = forwardRef<ShipModelRef>(
         )
     }
 )
-
 
 const Ship = () => {
     const camera = useRef<THREE.PerspectiveCamera>(null)
