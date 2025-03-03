56b839143035f9eb9b6dc1dc3548d629b94498f6
Fzgt <fzgt320@gmail.com>
2025-03-03 11:47:42 +1100

    refactor: ship.tsx & integrate zustand

--- src/components/Ship.tsx
+++ src/components/Ship.tsx
@@ -1,57 +1,58 @@
 import { useAnimations, useGLTF, PerspectiveCamera, } from "@react-three/drei"
 import { Suspense, useRef, useEffect, useImperativeHandle, forwardRef } from "react"
+import { useBox } from "@react-three/cannon"
 import * as THREE from 'three'
 import { useFrame } from "@react-three/fiber"
-import { shipSpeed } from "../constants"
+import { useStore } from "../store"
 
 interface ShipModelRef {
     shipModel: React.RefObject<THREE.Group>
 }
 
-const ShipModel = forwardRef<ShipModelRef>(
+const ShipModel = forwardRef<ShipModelRef>((_props, ref) => {
 
-    (_props, ref) => {
-        const groupRef = useRef<THREE.Group>(null)
+    const { shipPosition, moveShip } = useStore();
+    const [groupRef, _api] = useBox<THREE.Group>(() => ({
+        position: shipPosition,
+        mass: 0,
+    }), useRef(null), [shipPosition]);
 
-        const { scene, animations } = useGLTF('/models/bull_dog/scene.gltf')
-        const { actions, names } = useAnimations(animations, groupRef)
-        useEffect(() => {
-            actions[names[0]]?.play();
-        }, []);
+    const { scene, animations } = useGLTF('/models/bull_dog/scene.gltf')
+    const { actions, names } = useAnimations(animations, groupRef)
+    useEffect(() => {
+        actions[names[0]]?.play();
+    }, []);
 
-        useImperativeHandle(ref, () => ({
-            shipModel: groupRef
-        }))
+    useImperativeHandle(ref, () => ({
+        shipModel: groupRef
+    }))
 
-        useFrame(() => {
-            groupRef.current?.position.set(0, 3, groupRef.current!.position.z - shipSpeed);
-        })
+    useFrame(() => {
+        moveShip();
+    })
 
-        return (
-            <group ref={groupRef} scale={2} position={[0, 1, -20]}>
-                <primitive
-                    object={scene}
-                    rotation={[0, Math.PI, 0]}
-                />
-            </group>
-        )
-    }
+    return (
+        <group ref={groupRef} scale={2}>
+            <primitive
+                object={scene}
+                rotation={[0, Math.PI, 0]}
+            />
+        </group>
+    )
+}
 )
 
 const Ship = () => {
     const camera = useRef<THREE.PerspectiveCamera>(null)
     const ShipModelRef = useRef<ShipModelRef>(null)
+    const shipPosition = useStore(state => state.shipPosition);
 
-    useFrame(() => {
-        if (!ShipModelRef) return;
-        if (!camera) return;
+    useEffect(() => {
+        if (!camera.current) return;
+        const [x, y, z] = shipPosition;
+        camera.current!.position.set(x, y + 4, z + 10);
 
-        const position = ShipModelRef.current?.shipModel.current?.position;
-        if (position) {
-            const { x, y, z } = position;
-            camera.current!.position.set(x, y + 4, z + 10);
-        }
-    })
+    }, [shipPosition])
 
     return (
         <>
