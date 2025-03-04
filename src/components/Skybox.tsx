621ee77ca70979d63f3aa267edede66b322febb7
Fzgt <fzgt320@gmail.com>
2025-03-04 22:11:14 +1100

    feat: skybox auto follow

--- src/components/Skybox.tsx
+++ src/components/Skybox.tsx
@@ -1,18 +1,26 @@
 import * as THREE from 'three';
 import { useTexture, Stars } from '@react-three/drei'
-import { useLayoutEffect } from 'react';
-
+import { useLayoutEffect, useRef, useEffect } from 'react';
+import { useStore } from '../store';
 
 const Skybox = () => {
 
     const galaxyTexture = useTexture('/textures/galaxy.jpg')
+    const skyboxRef = useRef<THREE.Group>(null);
+    const shipPosition = useStore(state => state.shipPosition);
+
     useLayoutEffect(() => {
         galaxyTexture.wrapS = galaxyTexture.wrapT = THREE.RepeatWrapping;
         galaxyTexture.repeat.set(2, 2);
     })
 
+    useEffect(() => {
+        if (!skyboxRef.current) return;
+        skyboxRef.current.position.set(...shipPosition);
+    }, shipPosition)
+
     return (
-        <>
+        <group ref={skyboxRef}>
             <Stars factor={40} radius={280} saturation={0} count={9000} fade speed={2} />
 
             <mesh>
@@ -24,7 +32,7 @@ const Skybox = () => {
                     map={galaxyTexture}
                 />
             </mesh>
-        </>
+        </group>
     )
 }
 
