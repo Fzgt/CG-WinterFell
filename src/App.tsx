25e2eb05d03bdc5cb4843072ebc845e612aef629
shy <849227667@qq.com>
2025-04-07 19:53:42 +1000

    feat: import pumpkins with reuseable Monster component

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,40 @@ import { Loader } from '@react-three/drei';
 import Skybox from './components/Skybox';
 import Ground from './components/Ground';
 import Ship from './components/Ship';
+import { randomInRange } from './utils';
 import Cubes from './components/Cubes';
+import Monsters from './components/Monsters';
 
 const App = () => (
     <>
         <Canvas>
             <ambientLight />
+            {/* Directional light (simulates sun, casts shadows) */}
+            <directionalLight
+                castShadow
+                position={[50, 100, 100]}
+                intensity={1.5}
+                shadow-mapSize-width={1024}
+                shadow-mapSize-height={1024}
+            />
             {/* <OrbitControls /> */}
             <Skybox />
 
             <Physics>
                 <Ground />
                 <Ship />
-                <Cubes />
+                <Monsters
+                    _path='/models/monsters/halloween_pumpkin_2.glb'
+                    rotation={[0, 0, 0]}
+                    scale={0.07}
+                    Yposition={2}
+                />
+                <Monsters
+                    _path='/models/monsters/halloween_pumpkin.glb'
+                    rotation={[0, 0, 0]}
+                    scale={10}
+                    Yposition={9}
+                />
             </Physics>
         </Canvas>
         <Loader />
