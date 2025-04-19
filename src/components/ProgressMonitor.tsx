127c156b203e1f4d68d7be7a56ab9e0b6d639dcf
litinguyen <160822907+litinguyen@users.noreply.github.com>
2025-04-19 21:03:01 +1000

    feat: welcomePage & preload models and textures

---
+++ src/components/ProgressMonitor.tsx
@@ -0,0 +1,22 @@
+import { useProgress } from '@react-three/drei';
+import { useEffect } from 'react';
+
+
+interface ProgressMonitorProps {
+    onProgress: (loaded: boolean) => void;
+}
+
+const ProgressMonitor = ({ onProgress }: ProgressMonitorProps) => {
+    console.log('progress-monitor triggered');
+    const { progress } = useProgress();
+
+    useEffect(() => {
+        if (progress >= 100) {
+            onProgress(true);
+        }
+    }, [progress, onProgress]);
+
+    return null;
+};
+
+export default ProgressMonitor;
--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,52 @@ export const shipSpeed = 0.6;
 export const cubeSize = 20;
 export const cubeCount = 40;
 
+export const monsters = [
+    {
+        path: '/models/monsters/halloween_pumpkin_2.glb',
+        scale: 0.07,
+        yPosition: 0
+    },
+    {
+        path: '/models/monsters/halloween_pumpkin.glb',
+        scale: 10,
+        yPosition: 8
+    },
+    {
+        path: '/models/monsters/emerald_bat.glb',
+        scale: 0.4,
+        yPosition: 28
+    },
+    {
+        path: '/models/monsters/glow_bat.glb',
+        scale: 0.3,
+        yPosition: 33
+    },
+    {
+        path: '/models/monsters/king_boo.glb',
+        scale: 0.03,
+        yPosition: 10
+    }
+];
+
+// models that need to be preloaded declared here
+export const MODEL_PATHS = [
+    '/models/monsters/halloween_pumpkin_2.glb',
+    '/models/monsters/halloween_pumpkin.glb',
+    '/models/monsters/emerald_bat.glb',
+    '/models/monsters/glow_bat.glb',
+    '/models/monsters/king_boo.glb',
+    '/models/jo_on_bike__rigged__animated/scene.gltf',
+    '/models/skybox/skybox.glb'
+];
+
+// textures that need to be preloaded declared here
+export const TEXTURE_PATHS = [
+    '/textures/tile.jpg',
+    '/textures/welcome-background.jpg',
+    '/textures/welcome-background-2.jpg'
+];
+
 export const wallRadius = 40
 export const leftBound = (-planeSize / 2) * 0.6
 export const rightBound = (planeSize / 2) * 0.6
