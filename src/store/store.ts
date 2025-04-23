2ab5f78a14b8ee1456efe3f04dbc42c55211e7b7
Fzgt <fzgt320@gmail.com>
2025-04-23 18:27:41 +1000

    style: prettier format & check webgpu support

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,31 +1,33 @@
 import { create } from 'zustand';
 import { GameStore } from '../types/store';
 
-export const useStore = create<GameStore>((set) => ({
+export const useStore = create<GameStore>(set => ({
     playerSpeed: 12,
-    addPlayerSpeed: () => set(state => {
-        const newSpeed = state.playerSpeed + 4;
-        console.log("New player speed:", newSpeed);
-        return { playerSpeed: newSpeed };
-    }),
+    addPlayerSpeed: () =>
+        set(state => {
+            const newSpeed = state.playerSpeed + 5;
+            console.log('New player speed:', newSpeed);
+            return { playerSpeed: newSpeed };
+        }),
 
     gameStarted: false,
-    setGameStarted: (started) => set({ gameStarted: started }),
+    setGameStarted: started => set({ gameStarted: started }),
 
     gameOver: false,
-    setGameOver: (over) => set({ gameOver: over }),
+    setGameOver: over => set({ gameOver: over }),
 
     gamePaused: false,
     togglePause: () => set(state => ({ gamePaused: !state.gamePaused })),
 
     score: 0,
-    addScore: (points) => set((state) => {
-        if (!state.gameOver && !state.gamePaused) {
-            return { score: state.score + points };
-        }
-        return state;
-    }),
+    addScore: points =>
+        set(state => {
+            if (!state.gameOver && !state.gamePaused) {
+                return { score: state.score + points };
+            }
+            return state;
+        }),
 
     playerPosition: [0, 1, -20], // player initial position
-    setPlayerPosition: (position) => set({ playerPosition: position })
+    setPlayerPosition: position => set({ playerPosition: position }),
 }));
