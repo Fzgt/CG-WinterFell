aeacbc0ef52839a5ddfa7fcf3e5bdf3c96fecf3c
Fzgt <62455458+Fzgt@users.noreply.github.com>
2025-04-24 16:57:55 +1000

    chore: add isDev & print building meta

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,11 @@
 export const randomInRange = (from: number, to: number) => {
     return Math.floor(Math.random() * (to - from + 1)) - to;
-}
+};
 
-export const randomInRange2 = (from: number, to: number) =>
-    from + Math.random() * (to - from);
+export const randomInRange2 = (from: number, to: number) => from + Math.random() * (to - from);
+
+console.log('env :', import.meta.env);
+export const isDev = import.meta.env?.DEV || import.meta.env?.MODE === 'development' || false;
 
 export const updateHighScores = (newScore: number) => {
     const highScoresStr = localStorage.getItem('winterfell-high-scores');
